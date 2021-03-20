f5a4bc755159d82d44dc315fbda37a7d5f630065
Tomáš Lepič <43825554+lepi1@users.noreply.github.com>
2021-03-20 21:43:45 +0100

    [MOD] auth, login, register (#1)
    * [ADD] auth routes
    
    * [ADD] auth templates, register and login works

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,12 @@
 import os
 
+from flask_bootstrap import Bootstrap
 from flask import Flask
 
 def create_app(test_config=None):
   app = Flask(__name__, instance_relative_config=True)
+  bootstrap = Bootstrap(app)
+
   app.config.from_mapping(
     SECRET_KEY='dev',
     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
