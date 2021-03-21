531b8bd060c0fa2362998bd898f3c8e8bdf2206d
lepi1 <tomaslepicg@gmail.com>
2021-03-21 09:29:29 +0100

    [MOD] make app installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+  name='daily_checklist',
+  version='1.0.0',
+  packages=find_packages(),
+  include_package_data=True,
+  zip_safe=False,
+  install_requires=[
+    'Flask >= 1.1.0',
+    'wheel',
+    'bootstrap-flask'
+  ],
+)
